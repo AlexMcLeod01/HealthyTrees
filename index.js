86b3bbe081139417db298ca290c9ba62e1e6e5bc
Alex McLeod <AlexMcLeod01@gmail.com>
2018-02-12 19:46:18 -0600

    Fixing build process

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 var path = require('path');
 
+var Port = process.env.PORT || 8080;
 // Define the port to run on
-app.set('port', 3000);
+app.set('port', Port);
 
 app.use(express.static(path.join(__dirname, 'build')));
 
